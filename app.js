76dce67f81a07e5b66e254f22f4b1bfae97d1182
Glenn Packer <glenn.x.packer@gsk.com>
2023-10-01 11:11:55 +0200

    morgan api logger

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import chalk from 'chalk';
+import morgan from 'morgan';
 
 const app = express();
+app.use(morgan(':method :url :status'));
 
 app.get('/', (req, res, next) => { 
   return res.status(200).send('hi');
 });
 
 app.listen(3210, ()=> {
-  console.log('listening on port: ' + chalk.green('3210'));
+  console.log(chalk.green('Listening On Port: 3210'));
 })
